6091ab471f6b0dcd24946896b95a60c21d443abc
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2024-08-16 12:23:07 +0330

    feat(envs): diffrent stage files

--- docker/scripts/backend.sh
+++ docker/scripts/backend.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-echo 'Running Server'
 if [[ $ENVIRONMENT == "PRODUCTION" ]]; then
+  echo "Running Server in Production Mode"
   fastapi run src/main.py --host 0.0.0.0 --port 8000
 else
+  echo "Running Server in Development Mode"
   fastapi dev src/main.py --host 0.0.0.0 --port 8000
 fi
