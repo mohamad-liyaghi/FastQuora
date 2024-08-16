0de9489b153e02f634bbfceab0adf130e80058ed
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2024-08-16 12:01:30 +0330

    feat(core): main file

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI(
+    title="Fast Quora",
+    description="FastApi Quora Project",
+    version="1.0.0",
+    docs_url="/",
+    redoc_url="/redoc/",
+)
+
+
+if __name__ == "__main__":
+    uvicorn.run(host="0.0.0.0", app="main:app", reload=True, workers=1)
