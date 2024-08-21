72e559459e944b8b5346fdf02298ea6f044f7203
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2024-08-21 12:14:52 +0330

    feat(elastic-repository): elastic record

--- tests/fixtures/repositories/elastic.py
+++ tests/fixtures/repositories/elastic.py
@@ -6,3 +6,14 @@ from core.repositories import ElasticRepository
 @pytest_asyncio.fixture(scope="class")
 async def elastic_repository():
     return ElasticRepository(User)
+
+
+@pytest_asyncio.fixture(scope="class")
+async def elastic_record(elastic_repository, user):
+    return await elastic_repository.create(
+        {
+            "id": user.id,
+            "email": user.email,
+            "nickname": user.nickname,
+        }
+    )
