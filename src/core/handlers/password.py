0ae80215ccd5c1e3c734620c1269694ade5e5568
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2024-08-24 12:20:45 +0330

    feat(handlers): hash strategy in testing environment

--- src/core/handlers/password.py
+++ src/core/handlers/password.py
@@ -1,4 +1,5 @@
 from passlib.context import CryptContext
+import os
 
 context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -6,8 +7,12 @@ context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 class PasswordHandler:
     @staticmethod
     async def hash_password(password: str) -> str:
+        if os.getenv("TESTING", "0") == "1":
+            return password[::-1]
         return context.hash(password)
 
     @staticmethod
     async def verify_password(hashed_password: str, password: str) -> bool:
+        if os.getenv("TESTING", "0") == "1":
+            return hashed_password == password[::-1]
         return context.verify(password, hashed_password)
