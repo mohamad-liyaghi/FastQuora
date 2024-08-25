b949cdbb2484c416276a32f3d17558bf384e915d
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2024-08-25 13:00:10 +0330

    feat(profiles): test for controller

--- tests/fixtures/controllers/__init__.py
+++ tests/fixtures/controllers/__init__.py
@@ -1,4 +1,5 @@
 from .base import base_controller
 from .auth import auth_controller
+from .profile import profile_controller
 
-__all__ = ["base_controller", "auth_controller"]
+__all__ = ["base_controller", "auth_controller", "profile_controller"]
