59e91a6a2db1042c65d4532bedf4d96c0e6a8f2f
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2024-08-25 12:12:35 +0330

    feat(profiles): route

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from .auth import router as auth_router
+from .profile import router as profile_router
 
 v1_router = APIRouter()
 v1_router.include_router(auth_router, prefix="/auth")
+v1_router.include_router(profile_router, prefix="/profile")
