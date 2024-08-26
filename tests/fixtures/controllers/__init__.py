231a60a9f4de8b4adde87593f73194909ca6723c
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2024-08-26 12:13:14 +0330

    feat(questions): controller fixture

--- tests/fixtures/controllers/__init__.py
+++ tests/fixtures/controllers/__init__.py
@@ -1,5 +1,11 @@
 from .base import base_controller
 from .auth import auth_controller
 from .profile import profile_controller
+from .question import question_controller
 
-__all__ = ["base_controller", "auth_controller", "profile_controller"]
+__all__ = [
+    "base_controller",
+    "auth_controller",
+    "profile_controller",
+    "question_controller",
+]
