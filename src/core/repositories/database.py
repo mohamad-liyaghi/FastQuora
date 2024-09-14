c73eccf1cfff832fa5cde1e0eb662d39977784ec
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2024-09-14 13:09:36 +0330

    fix(typing): database repository typing

--- src/core/repositories/database.py
+++ src/core/repositories/database.py
@@ -2,8 +2,11 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy.orm import selectinload
 from sqlalchemy.orm.exc import NoResultFound
-from typing import Type, List, Optional
+from typing import Type, List, Optional, TypeVar, Union, cast
+from sqlalchemy.engine import Result
 from core.database import Base
+
+T = TypeVar("T", bound=Base)
 
 
 class DatabaseRepository:
@@ -11,11 +14,11 @@ class DatabaseRepository:
     This class is responsible for handling the database operations.
     """
 
-    def __init__(self, model: Type[Base], session: AsyncSession):
+    def __init__(self, model: Type[T], session: AsyncSession):
         self.model = model
         self.session = session
 
-    async def create(self, data: dict) -> Type[Base]:
+    async def create(self, data: dict) -> T:
         record = self.model(**data)
         self.session.add(record)
         await self.session.commit()
@@ -23,7 +26,7 @@ class DatabaseRepository:
 
     async def retrieve(
         self, join_fields: Optional[List[str]] = None, many: bool = False, **kwargs
-    ) -> Type[Base] | List[Type[Base]] | None:
+    ) -> Union[T, List[T], None]:
         query = select(self.model)
 
         if join_fields:
@@ -31,22 +34,24 @@ class DatabaseRepository:
                 query = query.options(selectinload(getattr(self.model, field)))
 
         query = query.filter_by(**kwargs)
-        result = await self.session.execute(query)
+        result: Result = await self.session.execute(query)
 
         if many:
-            return result.scalars().all()
+            # Explicitly casting the result to List[T]
+            return cast(List[T], result.scalars().all())
         else:
             try:
-                return result.scalars().first()
+                # Explicitly casting the result to T
+                return cast(T, result.scalars().first())
             except NoResultFound:
                 return
 
-    async def update(self, instance: Type[Base], data: dict) -> Type[Base]:
+    async def update(self, instance: T, data: dict) -> T:
         for key, value in data.items():
             setattr(instance, key, value)
         await self.session.commit()
         return instance
 
-    async def delete(self, instance: Type[Base]) -> None:
+    async def delete(self, instance: T) -> None:
         await self.session.delete(instance)
         await self.session.commit()
