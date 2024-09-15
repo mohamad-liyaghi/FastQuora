fb23ce5f86def2cbd1d8633e8645caac0fd2f5bf
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2024-09-15 14:00:24 +0330

    feat(celery-beat): cron insert vote task

---
+++ src/app/tasks/vote.py
@@ -0,0 +1,32 @@
+from celery import shared_task
+from app.controllers.vote import VoteController
+from core.database import get_db
+from core.redis import get_redis
+import asyncio
+
+
+@shared_task
+def auto_insert_vote_from_cache_to_db():
+    """
+    This task will insert the votes from cache to the database periodically.
+    """
+    cache_key = "vote:*:*"
+    redis = asyncio.run(get_redis())
+    db_gen = get_db()
+    db = asyncio.run(db_gen.__anext__())
+
+    try:
+        vote_controller = VoteController(session=db, redis_session=redis)
+        all_cached_votes = asyncio.run(redis.keys(cache_key))
+
+        # Filter the records which have "source" as "cache"
+        new_records = [record for record in all_cached_votes if record["source"] == "cache"]
+        if not new_records:
+            return "No new votes to insert from cache to database."
+
+        asyncio.run(vote_controller.bulk_create(new_records))
+        asyncio.run(redis.delete(cache_key))
+        return f"Inserted {len(new_records)} votes from cache to database."
+
+    finally:
+        asyncio.run(db_gen.aclose())
--- src/core/celery/base.py
+++ src/core/celery/base.py
@@ -1,7 +1,15 @@
 from celery import Celery
+from celery.schedules import crontab
 from core.settings import settings
 
 celery = Celery(__name__, broker=settings.CELERY_BROKER_URL)
 celery.conf.result_backend = settings.CELERY_RESULT_BACKEND
 
-celery.autodiscover_tasks()
+celery.autodiscover_tasks(["app.tasks"])
+
+celery.conf.beat_schedule = {
+    "auto_insert_vote_from_cache_to_db": {
+        "task": "app.tasks.vote.auto_insert_vote_from_cache_to_db",
+        "schedule": crontab(minute="*/1"),
+    }
+}
